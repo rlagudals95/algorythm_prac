ccbeab02d3a641137b132a77b0efb3f431afeff7
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-06-09 15:04:43 +0900

    화이팅

--- JS/땅따먹기.js
+++ JS/땅따먹기.js
@@ -28,3 +28,40 @@ const sum = array.reduce((acc, cur) => {
 const sum2 = array.reduce((acc, cur,) => {
     return acc + cur
 })
+
+//////////// 내 풀이
+
+function solution(land) {
+  var answer = 0;
+
+  let maxIdx = [];
+
+  for (let i = 0; i < land.length; i++) {
+    let maxNum = Math.max.apply(null, land[i]); // 가장 큰 수를 찾는다
+    let idx = land[i].findIndex((item) => item === maxNum);
+    maxIdx.push(idx);
+  }
+
+  // console.log(maxIdx)
+  //.. 두번째 큰수의..흠..
+
+  for (let j = 0; j < maxIdx.length; j++) {
+    if (maxIdx[j] === maxIdx[j + 1]) {
+      land[j + 1].splice(maxIdx[j + 1], 1, 0);
+    }
+  } // idx가 겹치면 그부분을 없애주고 0으로 만들어준다!
+
+  let resultNums = [];
+
+  for (let x = 0; x < land.length; x++) {
+    let resultMax = Math.max.apply(null, land[x]);
+    resultNums.push(resultMax);
+  }
+
+  let result = resultNums.reduce((acc, cur) => {
+    // 최종적으로 idx가 겹쳐지 않은 큰 수들의 합을 구한다
+    return acc + cur;
+  });
+
+  return result;
+}
